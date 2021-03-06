eddf107630894b5dba938e1464c4c5fd86009622
João Guedes <48497893+56Aang@users.noreply.github.com>
2021-03-06 17:56:23 +0000

    init

---
+++ src/generator.cpp
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    float x,y,z;
+
+    if(argc == 3 && strcmp(argv[1],"plane")){ // plano
+
+    }
+    else if (argc == 6 && strcmp[1],"sphere"){ // esfera
+    
+    }
+    else if ((argc == 6 || argc == 7)&& strcmp[1],"box"){ // box
+    
+    }
+    else if (argc == 7 && strcmp[1],"cone"){ // cone
+    
+    }
+    return 1;
+}
