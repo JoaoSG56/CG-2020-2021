3047115adbd796b83911068aff2bb851d030b679
56Aang <a89588@alunos.uminho.pt>
2021-03-06 19:49:22 +0000

    initial

--- src/generator.cpp
+++ src/generator.cpp
@@ -1,20 +1,32 @@
-#include <math.h>
-#include <string.h>
+#include <cmath>
+#include <cstring>
 
-int main(int argc, char *argv[]){
-    float x,y,z;
+int main(int argc, char *argv[]) {
+    float x, y, z;
+    float radius, height;
+    int slices,stacks, optional;
 
-    if(argc == 3 && strcmp(argv[1],"plane")){ // plano
+    if (argc == 3 && strcmp(argv[1], "plane") == 0) { // plano
 
-    }
-    else if (argc == 6 && strcmp[1],"sphere"){ // esfera
-    
-    }
-    else if ((argc == 6 || argc == 7)&& strcmp[1],"box"){ // box
-    
-    }
-    else if (argc == 7 && strcmp[1],"cone"){ // cone
-    
+    } else if (argc == 6 && strcmp(argv[1], "sphere") == 0) { // esfera
+        radius = atof(argv[2]);
+        slices = atoi(argv[3]);
+        stacks = atoi(argv[4]);
+
+
+    } else if ((argc == 6 || argc == 7) && strcmp(argv[1], "box") == 0) { // box
+        x = atof(argv[2]);
+        y = atof(argv[3]);
+        z = atof(argv[4]);
+
+        optional = atoi(argv[5]); // else = -1
+
+
+    } else if (argc == 7 && strcmp(argv[1], "cone") == 0) { // cone
+        radius = atof(argv[2]);
+        height = atof(argv[3]);
+        slices = atoi(argv[4]);
+        stacks = atoi(argv[5]);
     }
     return 1;
 }
